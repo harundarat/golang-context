7b65cb366baa048ec94416c5f47a3c807b607b5f
harundarat <harundarat@gmail.com>
2025-07-11 08:41:20 +0700

    context with value

--- context_test.go
+++ context_test.go
@@ -13,3 +13,29 @@ func TestContext(t *testing.T) {
 	todo := context.TODO()
 	fmt.Println("TODO context:", todo)
 }
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+
+	fmt.Println(contextF.Value("f")) // Output: F
+	fmt.Println(contextF.Value("c")) // Output: C
+	fmt.Println(contextF.Value("b")) // Output: <nil> -> contextB is not an ancestor of contextF
+
+	fmt.Println(contextA.Value("b")) // Output: <nil> -> contextB is not an ancestor of contextA
+
+}
